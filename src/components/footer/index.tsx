a83320b7bb218ae3ed26551d90d139caa4a29abe
shakizwastaken <rheyahyathebboss@gmail.com>
2023-09-08 21:37:56 +0100

    mama changes

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,5 +1,35 @@
+import Logo from "../ui/logo";
+
 export default function Footer() {
   return (
-    <footer className="min-h-[20vh] w-full bg-primary text-primary-foreground"></footer>
+    <footer className="card card-solo flex min-h-[20vh] w-full items-center justify-center bg-primary text-card-foreground">
+      <div className="container flex h-full items-center justify-start [&>*]:flex-[50%]">
+        <div className="flex h-full flex-col items-start justify-start gap-4">
+          <Logo
+            onClick={() => {
+              window.scrollTo({
+                top: 0,
+                behavior: "smooth",
+              });
+            }}
+            className="h-[25px] opacity-50"
+          />
+          <p className="text-sm opacity-50">
+            Conçu et développé par FinaCards © 2017, Tous droits réservés.
+          </p>
+        </div>
+
+        <div>
+          <div className="text-sm opacity-50">
+            <p>
+              Adresse: Parc Industriel CFCIM, Lot 92, N 9، Bouskoura 20287,
+              Maroc
+            </p>
+            <p> Email: commercial@finacards.ma </p>
+            <p> Téléphone: + 212 5 22-92-56-81</p>
+          </div>
+        </div>
+      </div>
+    </footer>
   );
 }
