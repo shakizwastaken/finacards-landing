b1c23b056db28e53c286bc376aae8484c30425d2
shakizwastaken <rheyahyathebboss@gmail.com>
2023-09-11 18:44:57 +0100

    fixed form success mobile

--- src/features/contact-us/success.tsx
+++ src/features/contact-us/success.tsx
@@ -5,9 +5,9 @@ import Link from "next/link";
 
 export default function ContactUsSuccessPage() {
   return (
-    <NavbarLayout className="flex h-[calc(100vh-var(--navbar-height))] flex-col items-center justify-center gap-4">
+    <NavbarLayout className="flex h-[calc(100vh-var(--navbar-height))] flex-col items-center justify-center gap-4 text-center">
       <CheckCircleIcon className="h-[50%] w-[50%] text-primary" />
-      <p className="text-3xl font-bold text-primary">
+      <p className="text-lg font-bold text-primary md:text-3xl">
         Votre message a bien été envoyé !
       </p>
       <Link href={"/"}>
