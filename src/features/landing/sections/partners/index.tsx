78583f5286270b6f7eefb900dcf6bfd42c3039e8
shakizwastaken <rheyahyathebboss@gmail.com>
2023-09-18 14:53:17 +0100

    updated partners

--- src/features/landing/sections/partners/index.tsx
+++ src/features/landing/sections/partners/index.tsx
@@ -2,21 +2,12 @@ import partners from "./partners";
 
 export default function LandingPartners() {
   const mapPartners = () =>
-    partners.map((Partner, i) => (
-      // eslint-disable-next-line @next/next/no-img-element
-      // <Partner
-      // className="w-fit md:flex-shrink [&>*]:fill-foreground"
-      // src={Partner}
-      // key={i}
-      // alt="a partner of finacards.ma"
-      // />
-      <Partner key={i} fill="white" />
-    ));
+    partners.map((Partner, i) => <Partner key={i} fill="white" />);
 
   return (
     <section
       id="partners"
-      className="container flex items-center justify-center gap-12 overflow-hidden py-20 md:py-16"
+      className="container flex items-center justify-center gap-12 overflow-hidden py-20 md:py-16 [&>*]:max-h-[40px] [&>*]:w-fit [&>*]:object-contain md:[&>*]:max-h-[75px]"
     >
       {mapPartners()}
     </section>
